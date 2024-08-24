79f088e8fb18d5d12f31c341e831898e2b8eeae4
Marcelo Jara <mjara@techk.cl>
2024-08-24 17:43:00 -0400

    format data response products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,13 +4,25 @@ const {
 
 async function getProducts(req, res) {
   try {
-    const products = await fetchProducts(req.body);
+    let products = await fetchProducts(req.body);
     if (!products.length) {
       return res.status(404).send({
         status: 404, 
         response: 'Not found Product.',
       });
     }
+    products = products.map(prod => {
+      return {
+        id: prod.id,
+        title: prod.title.toLowerCase(),
+        image: prod.image,
+        price: prod.price,
+        category: {
+          id: prod.categories.id,
+          name: prod.categories.description,
+        },
+      };
+    })
     return res.status(200).send({status: 200, response: products});
   } catch (error) {
     return res.status(500).send({
