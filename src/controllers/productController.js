4ea50b3011100807efb8d207336a60661df147d9
Marcelo Jara <mjara@techk.cl>
2024-08-24 17:51:54 -0400

    apply format data products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,32 +4,38 @@ const {
 
 async function getProducts(req, res) {
   try {
-    let products = await fetchProducts(req.body);
+    const products = await fetchProducts(req.body);
     if (!products.length) {
       return res.status(404).send({
         status: 404, 
         response: 'Not found Product.',
       });
     }
-    products = products.map(prod => {
-      return {
-        id: prod.id,
-        title: prod.title.toLowerCase(),
-        image: prod.image,
-        price: prod.price,
-        category: {
-          id: prod.categories.id,
-          name: prod.categories.description,
-        },
-      };
-    })
-    return res.status(200).send({status: 200, response: products});
+    return res.status(200).send({
+      status: 200, 
+      response: formatResponse(products),
+    });
   } catch (error) {
     return res.status(500).send({
       status: 500, 
       message: 'Internal server error.',
     });
   }
+};
+
+function formatResponse(response) {
+  return response.map(prod => {
+    return {
+      id: prod.id,
+      title: prod.title.toLowerCase(),
+      image: prod.image,
+      price: prod.price,
+      category: {
+        id: prod.categories.id,
+        name: prod.categories.description,
+      },
+    };
+  })
 };
 
 module.exports = {
