297576a76df3420575dcd811e9b309ab4945edf7
TheSumsito <43194508+TheSumsito@users.noreply.github.com>
2024-08-24 02:31:29 -0400

    create function fetchAssembled

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,4 +1,25 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-module.exports = prisma;
+async function fetchAssembled(payload={}) {
+  const query = {
+    where: payload,
+    include: {
+      assembled_products: {
+        include: {
+          products: {
+            include: {
+              categories: true,
+            },
+          },
+        },
+      },
+    },
+  };
+  if (!Object.keys(query).length) delete query.where;
+  return await prisma.assembled_computers.findMany(query);
+};
+
+module.exports = {
+    fetchAssembled,
+};
